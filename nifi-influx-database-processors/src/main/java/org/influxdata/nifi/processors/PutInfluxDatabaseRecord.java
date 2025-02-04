9e4c5c0672c1bf8578a7b500cf2ae06903435bb3
Oliver Schnabl <oliver.schnabl@drei.com>
2025-02-04 16:01:27 +0100

    remove evnet drive import

--- nifi-influx-database-processors/src/main/java/org/influxdata/nifi/processors/PutInfluxDatabaseRecord.java
+++ nifi-influx-database-processors/src/main/java/org/influxdata/nifi/processors/PutInfluxDatabaseRecord.java
@@ -37,7 +37,6 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.nifi.annotation.behavior.EventDriven;
 import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.SupportsBatching;
 import org.apache.nifi.annotation.behavior.WritesAttribute;
